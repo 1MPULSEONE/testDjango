db901d4ddc1816ece0a96612ee4bf674f171a4fb
1MPULSEONE <parkhomenko2049@gmail.com>
2021-05-13 18:10:12 +0600

    Big changes - Created about.html, news_article.html, news_list.html - Now all templates extends base.html - Index.html was renamed to base.html

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,6 +1,8 @@
-from django.contrib.auth import admin
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:article_id>/', views.get_article, name='article'),
+    path('about', views.about, name='about'),
 ]
--- test_app/views.py
+++ test_app/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 
+articles = [
+    {
+        'id': 1,
+        'title': 'First news',
+        'text': 'This is the worst article'
+    },
+    {
+        'id': 2,
+        'title': 'Second news',
+        'text': 'This is the amazing second article'
+    }]
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'test_app/index.html')
+    return render(request, 'test_app/news_list.html', {'articles': articles})
+
+
+def about(request):
+    return render(request, 'test_app/about.html')
+
+
+def get_article(request, article_id):
+    article = articles[article_id - 1]
+    return render(request, 'test_app/news_article.html', {'article': articleп})
